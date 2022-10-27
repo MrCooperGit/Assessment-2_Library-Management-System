6457009f066795c7a9d41400ca8994a52d82c61f
MrCooperGit <100725634+MrCooperGit@users.noreply.github.com>
2022-10-27 15:18:52 +1300

    Login Screen buttons and img loaded

--- loginWindow.cpp
+++ loginWindow.cpp
@@ -1,15 +1,27 @@
 #include "loginWindow.h"
 #include "./ui_loginWindow.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 LoginWindow::LoginWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
+    QPixmap pix(":/img/library.label.png");
+    int w = ui->label_img->width();
+    int h = ui->label_img->height();
+    ui->label_img->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio));
 }
 
 LoginWindow::~LoginWindow()
 {
     delete ui;
+}
+
+
+void LoginWindow::on_pushButton_clicked()
+{
+
 }
 
--- loginWindow.h
+++ loginWindow.h
@@ -15,6 +15,9 @@ public:
     LoginWindow(QWidget *parent = nullptr);
     ~LoginWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::LoginWindow *ui;
 };
