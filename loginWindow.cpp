1a16fe1534339cdeaf92ba4b8d8827c835666df5
MrCooperGit <100725634+MrCooperGit@users.noreply.github.com>
2022-11-16 15:07:37 +1300

    cleaned up loginWindow and added functions to write overdue and dueSoon files

--- loginWindow.cpp
+++ loginWindow.cpp
@@ -2,17 +2,8 @@
 #include "./ui_loginWindow.h"
 #include "classes.h"
 #include "admin_home_screen.h"
-#include "edit_member_screen.h"
-#include "edit_book_screen.h"
 #include "member_home_screen.h"
 #include "admin_home_screen.h"
-#include "admin_catalogue_screen.h"
-#include "member_catalogue_screen.h"
-#include "add_new_book_screen.h"
-#include "add_new_member_screen.h"
-#include "admin_book_view_screen.h"
-#include "your_items_screen.h"
-#include "member_book_order_screen.h"
 
 #include <QFile>
 #include <QTextStream>
@@ -45,6 +36,108 @@ LoginWindow::LoginWindow(QWidget *parent)
     connect(timer, SIGNAL(timeout()), this, SLOT(currentDate()));
     timer->start(1000); //time in ms
 
+    //[0] function to read all orders.csv 'due soon' and 'overdue'
+    //and write them in separate files respectively
+    //create/link file including validation checks
+    QDir current;
+    QString currentPath = current.currentPath(); //create string of current directory
+    QDir dir(currentPath); //QDir variable becomes current directory
+    QFile file(dir.filePath("orders.csv")); //file is now specific to the user's directory
+
+    if(!file.exists()) //create file if it doesn't exist
+    {
+        if (!file.open(QIODevice::ReadWrite))
+        {
+        qWarning("Cannot create the file");
+        }
+        file.close();
+    }
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        qCritical() << file.errorString();
+    }
+
+    //[1] Delete previous file versions to start fresh
+    dir.remove("overdue.csv");
+    dir.remove("dueSoon.csv");
+    //[1]
+
+    //[2] Create files in build path
+    QFile file2(dir.filePath("dueSoon.csv")); //file is now specific to the user's directory
+    if(!file2.exists()) //create file if it doesn't exist
+    {
+        if (!file2.open(QIODevice::ReadWrite))
+        {
+        qWarning("Cannot create the file");
+        }
+        file2.close();
+    }
+    QFile file3(dir.filePath("overdue.csv")); //file is now specific to the user's directory
+    if(!file3.exists()) //create file if it doesn't exist
+    {
+        if (!file3.open(QIODevice::ReadWrite))
+        {
+        qWarning("Cannot create the file");
+        }
+        file3.close();
+    }
+    //[2]
+
+
+    while (!file.atEnd()){
+
+        QString line = file.readLine(); //store each line from file
+        QString date, fileUserId, fileBookId;
+
+        Book newBook; //declare book object
+
+        //Making a string list to seperate each column of the file
+        QStringList fileList;
+        fileList.clear();
+        fileList = line.split(",");
+
+        //Transfer data from file into variables
+        fileBookId = fileList[0];
+        newBook.setTitle(fileList[1]);
+        fileUserId = fileList[2];
+        date = fileList[3];
+        if (date.contains("\n")) date.chop(1); //remove \n from last word in line
+        if (date.contains("\r")) date.chop(1); //remove \r from last word in line
+qInfo() << fileBookId << newBook.getTitle() << fileUserId << date;
+
+        //Compare current date with due dates from file
+        QDate currentDate = QDate::currentDate(), fileDate;
+        fileDate = QDate::fromString(date, "dd/MM/yyyy");
+        int num = currentDate.daysTo(fileDate);
+qInfo() << num;
+
+        //If book is due within 3 days, write in dueSoon.csv
+        if(num >= 0 && num <= 3){
+            if(!file2.open(QIODevice::Append))
+            {
+                qCritical() << file2.errorString();
+            }
+            QTextStream stream(&file2); //declare variable to store the stream from file
+
+            //stream pastes the variables into the .csv file with /n for new line and ',' for next cell
+            stream << fileBookId << "," << newBook.getTitle() << "," << fileUserId << "," << date << "\n";
+            file2.close();
+        }
+
+        //If book is overdue, write in overdue.csv
+        if (num < 0){
+            if(!file3.open(QIODevice::Append))
+            {
+                qCritical() << file3.errorString();
+            }
+            QTextStream stream(&file3); //declare variable to store the stream from file
+
+            //stream pastes the variables into the .csv file with /n for new line and ',' for next cell
+            stream << fileBookId << "," << newBook.getTitle() << "," << fileUserId << "," << date << "\n";
+            file3.close();
+        }
+    }
+    //[0]
 }
 
 LoginWindow::~LoginWindow()
@@ -61,7 +154,7 @@ void LoginWindow::currentDate(){
 
 void LoginWindow::on_pushButton_login_clicked()
 {
-    QString loginEmail = ui->lineEdit_username->text();
+    QString loginEmail = ui->lineEdit_username->text().toLower();
     QString loginPassword = ui->lineEdit_password->text();
 
     QDir current;
@@ -86,7 +179,7 @@ void LoginWindow::on_pushButton_login_clicked()
     QString email, password, firstName, lastName, userId;
     while (!file.atEnd()){
         QString line = file.readLine(); //place line into string
-    qInfo() << line;
+qInfo() << line;
         //empty the variables before each loop
         email.clear();
         password.clear();
@@ -97,19 +190,22 @@ void LoginWindow::on_pushButton_login_clicked()
 
         QStringList list = line.split(","); //split string into string array using "," as break
         //assign variables by index of the string array
-        email = list[0];
+        email = list[0].toLower();
         userId = list[1];
         password = list[5];
         firstName = list[2];
         lastName = list[3];
 
-        if (password.contains("\n")) password.chop(1);   //removes '\r\n' from password
+        if (password.contains("\n")) password.chop(1);   //removes '\n' from password
+        if (password.contains("\r")) password.chop(1);   //removes '\r' from password
 
         //using integers to validate login details
         //the compare() function returns 0 if strings are the same
         int validateEmail, validatePass;
         validateEmail = loginEmail.compare(email);
+qInfo() << "Validate Email: " << validateEmail;
         validatePass = loginPassword.compare(password);
+qInfo() << "Validate Password: " << validatePass;
 
         if(validateEmail==0 && validatePass==0){
 
@@ -207,10 +303,10 @@ void LoginWindow::on_pushButton_register_clicked()
     QMessageBox::critical(this, "Critical", "Please ensure all fields are entered!");
 }
 
-void LoginWindow::on_pushButton_temp_clicked()
+void LoginWindow::on_lineEdit_email_cursorPositionChanged(int arg1, int arg2)
 {
-    member_catalogue_screen *ptr = new member_catalogue_screen;
-    ptr->show();
-    close();
+    QMessageBox::information(this,"Information","The register function is displayed here with a radio button to choose\n"
+                                                "'member' or 'admin' for testing/marking purposes only.\n"
+                                                "This radio button would be removed for actual implementation.");
 }
 
